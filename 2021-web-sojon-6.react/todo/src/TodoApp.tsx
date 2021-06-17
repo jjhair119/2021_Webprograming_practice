5d1b58e57b2c79563c1cc7f21a347b7a32649923
jjhair119 <jmkjmk6666@gmail.com>
2021-06-17 17:29:16 +0900

    todolist css

---
+++ 2021-web-sojon-6.react/todo/src/TodoApp.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import TodoItem from "./components/TodoItem";
+
+interface TodoAppProps {}
+interface TodoAppState {
+  todoItems: string[];
+  newTodo: string;
+}
+
+class TodoApp extends React.Component<TodoAppProps, TodoAppState> {
+  constructor(props: TodoAppProps) {
+    super(props);
+    
+    this.state = {
+      todoItems: [],
+      newTodo: "",
+    };
+  }
+
+  handleNewTodo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      newTodo: e.target.value,
+    });
+  }
+
+  handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const items = this.state.todoItems.concat(this.state.newTodo)
+
+    this.setState({
+      todoItems: items,
+      newTodo: "",
+    })
+  }
+
+  render() {
+    return (
+      <div className = "divclass">
+        <h3>TO DO LIST</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="new-todo" className="headerclass">당신의 계획을 기록하세요</label> <br />
+          <input type="text" placeholder="계획 내용 입력" id="new-todo" value={this.state.newTodo} onChange={this.handleNewTodo} /> <br />
+          <button>#{this.state.todoItems.length + 1}번째 계획 기록</button>
+        </form>
+        {
+          this.state.todoItems.map((item, idx) => (
+            <TodoItem name={item} key={idx}/>
+          ))
+        }
+      </div>
+    )
+  }
+}
+
+export default TodoApp;
